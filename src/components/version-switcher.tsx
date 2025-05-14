120515a4764eb55935e499e9e982ffe55e87d606
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-05-14 05:32:02 +0000

    Update version-switcher.tsx

--- src/components/version-switcher.tsx
+++ src/components/version-switcher.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Check, ChevronsUpDown, GalleryVerticalEnd } from "lucide-react"
+import { Check, ChevronsUpDown } from "lucide-react"
 
 import {
   DropdownMenu,
