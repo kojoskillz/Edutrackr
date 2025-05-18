4bb133ded78d72ef1c7a8e9fc47bb0eaa863b10e
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-05-18 18:16:15 +0000

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -240,7 +241,15 @@ export default function Page() {
     }
   };
 
-  const { setPaidFees, setUnpaidFees } = useFeesStore();
+  const {  } = useFeesStore();
+
+  function setPaidFees(arg0: number): void {
+    throw new Error("Function not implemented.");
+  }
+
+  function setUnpaidFees(arg0: number): void {
+    throw new Error("Function not implemented.");
+  }
 
   return (
     <SidebarProvider>
