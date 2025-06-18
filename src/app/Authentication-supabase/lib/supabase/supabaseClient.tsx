130fa34e3c8ec899caa633e93296c5f992f9aa79
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-06-18 12:13:25 +0000

    Delete src/app/Authentication-supabase directory

--- src/app/Authentication-supabase/lib/supabase/supabaseClient.tsx
+++
@@ -1,7 +0,0 @@
-// lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://jhmmtqlwczucigkxbrtw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpobW10cWx3Y3p1Y2lna3hicnR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NjI3NTAsImV4cCI6MjA2NDQzODc1MH0.6ERjS-dMmD-KWLwQpQfcScPiMVkCpWI-Y1d3VH41mvw'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
