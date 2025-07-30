81cc4a43c149d712203f9de6b0332794c8a194b1
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-07-30 22:33:33 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,57 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-'use client';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { StudentProvider } from "@/context/StudentContext";
+import { AuthProvider } from "@/context/AuthContext";
+import { createServerSupabaseClient } from '../app/Authentication-supabase/lib/supabase/server';
+import SupabaseProvider from '../app/Authentication-supabase/lib/supabase/superbaseProvider';
+import  SupabaseListener  from '../app/Authentication-supabase/lib/supabase/superbaseProvider';
 
-import React, { useEffect, useState } from 'react';
-import Image from "next/image";
-import Link from 'next/link';
-import { supabase } from '../app/Authentication-supabase/lib/supabase/supabaseClient'; // Adjust the path as necessary'
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
-const Homepage = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const checkSession = async () => {
-      const { data } = await supabase.auth.getSession();
-      setIsLoggedIn(!!data.session);
-    };
-
-    checkSession();
-  }, []);
-
-  return (
-    <div className="grid m-auto items-center place-content-center min-h-screen bg-gradient-to-r from-blue-800 via-blue-600 to-blue-400 px-4 py-10 overflow-hidden">
-      <div className="bg-white shadow-2xl rounded-3xl w-full max-w-6xl grid place-content-center p-10">
-        <div className="flex flex-col md:flex-row items-center justify-between">
-          <div className="text-center md:text-left space-y-4 max-w-md mx-auto md:mx-0">
-            <div>
-              <Image
-                src="/edutrackrlogo.png"
-                width={300}
-                height={300}
-                alt="Edutrackr illustration"
-                className="object-coverrounded-lg"
-              />
-            <h3 className="text-2xl mt-[-5rem] font-semibold text-gray-700">
-              School Management System
-            </h3>
-            <p className="text-lg text-gray-500 mb-6">
-              A comprehensive platform for efficient school management, designed to streamline tasks and enhance learning.
-            </p>
-
-            <Link href={"/Authentication-supabase/login"}>
-              <button className="text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 w-[12rem] h-[3rem] font-semibold rounded-full shadow-md transform hover:scale-105">
-                Get Started
-              </button>
-            </Link>
-          </div>
-          </div>
-
-          <div className="mt-10 md:mt-0">
-            <Image
-              src="/img1.png"
-              width={500}
-              height={500}
-              alt="Edutrackr illustration"
-              className="object-cover h-[20rem] w-[20rem] md:h-[25rem] md:w-[25rem] rounded-lg"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export const metadata: Metadata = {
+  title: "Edutrackr",
+  description: "School Management System",
 };
 
-export default Homepage;
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createServerSupabaseClient();
+  const { data: { session } } = await supabase.auth.getSession();
+
+
+  return (
+    <html lang="en">
+        <head>
+            <link rel="icon" href="/edutrackrlogo1.png" className="rounded-lg m-32 w-32" />
+        </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ToastContainer />
+        <SupabaseProvider session={session}>
+          <SupabaseListener session={session}>
+            <AuthProvider>
+            <StudentProvider>
+              {children}
+            </StudentProvider>
+          </AuthProvider>
+        </SupabaseListener>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
